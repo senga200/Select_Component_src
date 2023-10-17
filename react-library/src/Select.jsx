cd89eb9215977853db191bd0a7cbacf34dcb576e
Agnès Cappello <senga.ds@gmail.com>
2023-10-17 22:11:10 +0200

    1.0.5

--- react-library/src/Select.jsx
+++ react-library/src/Select.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 
-function Select({ children, options }) {
+function Select({ options }) {
   return (
     <div className="selectComponent">
       <select>
         <option value="">Select in dropdown list</option>
-        {children ||
-          (options &&
-            options.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            )))}
+        {options &&
+          options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
       </select>
     </div>
   );
